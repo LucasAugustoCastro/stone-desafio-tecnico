0fa4aa04646536428bbd836e2ad20fe330d31f22
Lucas Castro <lucasaugustocastro24@gmail.com>
2024-12-13 22:41:59 -0300

    refactor: usando principios do SOLID

--- app/controllers/atendimentos.py
+++ app/controllers/atendimentos.py
@@ -1,40 +1,19 @@
-from sqlalchemy.exc import IntegrityError
-
-from app import db
-from app.models.atendimentos import Atendimento
+from app.repository.atendimentos import AtendimentoRepository
 
 class Atendimentos():
-  def listar_atendimentos(self, filters):
-    query = Atendimento.query
-    if 'tecnico' in filters:
-      query = query.filter_by(angel=filters['tecnico'])
-    if 'base_logistica' in filters:
-      query = query.filter_by(polo=filters['base_logistica'])
-    if 'data_limite_start' in filters and 'data_limite_end' in filters:
-      query = query.filter(Atendimento.data_limite.between(filters['data_limite_start'], filters['data_limite_end']))
-    if 'data_de_atendimento_start' in filters and 'data_de_atendimento_end' in filters:
-      query = query.filter(Atendimento.data_de_atendimento.between(filters['data_de_atendimento_start'], filters['data_de_atendimento_end']))
-    return query.all()
-  
-  def criar_atendimento(self, data):
-    try:
-      db.session.add(data)
-      db.session.commit()
-    except IntegrityError:
-      db.session.rollback()
-      raise ValueError("Erro de integridade no banco de dados.")
-    return data
+    def __init__(self, repository:AtendimentoRepository):
+        self.repository = repository
 
-  def atualizar_atendimento(self, id, data):
-    atendimento:Atendimento = Atendimento.query.get(id)
-    if not atendimento:
-      raise ValueError("Atendimento não encontrado.")
-    for attr in data:
-      if hasattr(atendimento, attr):
-        setattr(atendimento, attr, data[attr])
-    try:
-      db.session.commit()
-      return atendimento
-    except IntegrityError:
-      db.session.rollback()
-      raise ValueError("Erro de integridade no banco de dados.")
+    def listar_atendimentos(self, filters):
+        return self.repository.get_filtered(filters)
+  
+    def criar_atendimento(self, data):
+        self.repository.add(data)
+        return data
+
+    def atualizar_atendimento(self, id, data):
+        atendimento = self.repository.get_by_id(id)
+        if not atendimento:
+            raise ValueError("Atendimento não encontrado.")
+        self.repository.update(atendimento, data)
+        return atendimento
